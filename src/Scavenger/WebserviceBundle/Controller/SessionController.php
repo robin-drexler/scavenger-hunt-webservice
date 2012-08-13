7df07bc3be6c80b8e988f013778dfcdf95d5be60
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2012-08-13 11:29:06 +0200

    refactoring

--- src/Scavenger/WebserviceBundle/Controller/SessionController.php
+++ src/Scavenger/WebserviceBundle/Controller/SessionController.php
@@ -2,10 +2,10 @@
 
 namespace Scavenger\WebserviceBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use \Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use \Symfony\Component\HttpFoundation\Response;
 use \Symfony\Component\HttpFoundation\Request;
 use \Symfony\Component\HttpKernel\Exception\HttpException;
